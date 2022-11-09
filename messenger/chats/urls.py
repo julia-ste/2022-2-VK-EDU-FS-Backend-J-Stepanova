6918b5d24344d36ea45ebd0bf9c7f7fbfce0d3d0
Julia Stepanova <yuliya.stepanova@phystech.edu>
2022-11-09 11:20:37 +0300

    Upgrade views

--- messenger/chats/urls.py
+++ messenger/chats/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chat_list, name='chat_list'),
-    path('<int:pk>/', views.chat_detail, name='chat_detail'),
+    path('<int:user_pk>/', views.chat_list, name='chat_list'),
     path('new/', views.chat_create, name='chat_create'),
+    path('detail/<int:pk>/', views.chat_detail, name='chat_detail'),
+    path('update/<int:pk>/', views.chat_update, name='chat_update'),
+    path('delete/<int:pk>/', views.chat_delete, name='chat_delete'),
+
+    path("messages/<int:chat_pk>/", views.message_list, name="message_list"),
+    path("messages/<int:chat_pk>/new/", views.message_create, name="message_create"),
+    path("messages/detail/<int:pk>", views.message_detail, name="message_detail"),
+    path("messages/update/<int:pk>", views.message_update, name="message_update"),
+    path("messages/delete/<int:pk>", views.message_delete, name="message_delete"),
 ]
--- messenger/chats/views.py
+++ messenger/chats/views.py
@@ -1,30 +1,123 @@
-from django.http import JsonResponse, Http404
-from django.views.decorators.http import require_GET, require_POST
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_http_methods
 
-chats = [
-    {'id': 1, 'title': 'Chat 1'},
-    {'id': 2, 'title': 'Chat 2'},
-    {'id': 3, 'title': 'Chat 3'},
-    {'id': 4, 'title': 'Chat 4'},
-]
+from .models import Chat, Message, ChatMember
 
 
-@require_GET
-def chat_list(request):
-    return JsonResponse({'chats': chats})
+@require_http_methods(["GET"])
+def chat_list(request, user_pk):
+    chats = get_object_or_404(get_user_model(), id=user_pk).user_chats.values("id", "title")
+    return JsonResponse(
+        list(chats),
+        safe=False,
+        json_dumps_params={"ensure_ascii": False},
+        status=200
+    )
 
 
-@require_GET
-def chat_detail(request, pk):
-    if pk < 1 or len(chats) < pk:
-        raise Http404(f'Chat with id={pk} does not exist!')
-    return JsonResponse(chats[pk - 1])
+@require_http_methods(["GET"])
+def message_list(request, chat_pk):
+    messages = get_object_or_404(Chat, pk=chat_pk).chat_messages.values("id", "text")
+    return JsonResponse(
+        list(messages),
+        safe=False,
+        json_dumps_params={"ensure_ascii": False},
+        status=200
+    )
 
 
-@require_POST
+@require_http_methods(["POST"])
 def chat_create(request):
-    title = request.POST.get('title')
-    next_id = len(chats) + 1
-    new_chat = {'id': next_id, 'title': title}
-    chats.append(new_chat)
-    return JsonResponse(new_chat)
+    title = request.GET.get("title")
+    author_pk = request.GET.get("author")
+
+    author = get_object_or_404(get_user_model(), pk=author_pk)
+
+    chat = Chat.objects.create(title=title, author=author)
+    chat.save()
+
+    chat_member = ChatMember.objects.create(chat=chat, user=author)
+    chat_member.save()
+
+    return JsonResponse(
+        {'created chat': chat.title},
+        safe=False,
+        json_dumps_params={"ensure_ascii": False},
+        status=200
+    )
+
+
+@require_http_methods(["POST"])
+def message_create(request, chat_pk):
+    chat = get_object_or_404(Chat, pk=chat_pk)
+
+    text = request.GET.get("text")
+    user_pk = request.GET.get("user")
+
+    user = get_object_or_404(get_user_model(), id=user_pk)
+
+    message = Message.objects.create(chat=chat, user=user, text=text)
+    message.save()
+
+    return JsonResponse(
+        {'created message': message.text},
+        safe=False,
+        json_dumps_params={"ensure_ascii": False},
+        status=200
+    )
+
+
+@require_http_methods(["PATCH", "POST"])
+def chat_update(request, pk):
+    chat = get_object_or_404(Chat, pk=pk)
+    chat.title = request.GET.get("title")
+    chat.save()
+
+    return JsonResponse({"updated chat title": chat.title}, status=200)
+
+
+@require_http_methods(["PATCH", "POST"])
+def message_update(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+    message.text = request.GET.get("text")
+    message.save()
+
+    return JsonResponse({"updated message": message.text}, safe=False, status=200)
+
+
+@require_http_methods(["GET"])
+def chat_detail(request, pk):
+    chat = get_object_or_404(Chat, pk=pk)
+    return JsonResponse(
+        {'chat': chat.title},
+        safe=False,
+        json_dumps_params={"ensure_ascii": False},
+        status=200
+    )
+
+
+@require_http_methods(["GET"])
+def message_detail(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+    return JsonResponse(
+        {'message': message.text},
+        safe=False,
+        json_dumps_params={"ensure_ascii": False},
+        status=200
+    )
+
+
+@require_http_methods(["DELETE", "POST"])
+def chat_delete(request, pk):
+    chat = get_object_or_404(Chat, pk=pk)
+    chat.delete()
+    return JsonResponse({"deleted chat": chat.title}, safe=False, status=200)
+
+
+@require_http_methods(["DELETE", "POST"])
+def message_delete(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+    message.delete()
+    return JsonResponse({"deleted message": message.text}, safe=False, status=200)
