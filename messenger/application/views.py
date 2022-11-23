e795125911191629b889d47bf9e57927462b200b
Julia Stepanova <yuliya.stepanova@phystech.edu>
2022-11-23 06:25:49 +0300

    Prevent login when is authenticated

--- messenger/application/views.py
+++ messenger/application/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login as login_user
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 
 from utils.form_utils import add_field_placeholders
@@ -13,6 +14,8 @@ def home(request):
 
 @require_http_methods(["GET", "POST"])
 def login(request):
+    if request.user.is_authenticated:
+        return redirect(reverse("home"))
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         form = add_field_placeholders(form)
@@ -22,7 +25,7 @@ def login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login_user(request, user)
-                return redirect("home")
+                return redirect(reverse("home"))
         return render(
             request,
             "login.html",
